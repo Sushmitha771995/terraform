a2c1727f417a16b6dabf3d6462b983f2beef4223
sushmita <sushmita771995@gmail.com>
2021-02-11 17:38:30 +0530

    commit

---
+++ firstinstance.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIA4CH5VH3OWZNVVGPS"
+    secret_key = "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"
+  }
+
+  resource aws_instance "instance1" {
+    instance_type = "t2.micro"
+    ami = "ami-052ed3344670027b3"
+  }
+
