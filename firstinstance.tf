229995ecc7d9e252e3b225a345c4d8c364e36d9e
sushmita <sushmita771995@gmail.com>
2021-02-11 18:07:14 +0530

    commit

--- firstinstance.tf
+++ firstinstance.tf
@@ -1,8 +1,6 @@
-provider "aws" {
-    region = "us-east-1"
-    access_key = "AKIA4CH5VH3OVEUGM4HU"
-    secret_key = "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"
-  }
+
+
+
 
   resource "aws_instance" "instance1" {
     instance_type = "t2.micro"
