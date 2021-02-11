336295551f2aa219d8ddddbc057346c216139aeb
sushmita <sushmita771995@gmail.com>
2021-02-11 18:30:29 +0530

    commit

--- firstinstance.tf
+++ firstinstance.tf
@@ -3,6 +3,12 @@ output "hello" {
 
 }
 
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA4CH5VH3OVEUGM4HU"
+  secret_key = "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"
+}
+
 
 resource "aws_instance" "aws123" {
   ami = "ami-047a51fa27710816e"
